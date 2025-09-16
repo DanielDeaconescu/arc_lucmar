00f39b87f7a6b4b9f4a1c4a74a810f662c6c5253
Daniel Deaconescu <daniel.deaconescu98@gmail.com>
2025-09-16 10:07:29 +0300

    changed the styling for the active class in the navbar

--- components/navbar.php
+++ components/navbar.php
@@ -5,7 +5,7 @@
 ?>
 
 <nav class="navbar navbar-expand-lg fixed-top navbar-dark navbar-custom">
-    <div class="container">
+    <div class="container h-100">
         <a href="index.html" class="navbar-brand">
             Arc Lucmar
         </a>
@@ -14,22 +14,23 @@
             aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="menuLinks">
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                    <a href="index.php" class="nav-link <?= $current_page == 'index.php' ? 'active' : '' ?>">Acasă</a>
+        <div class="collapse navbar-collapse h-100" id="menuLinks">
+            <ul class="navbar-nav ms-auto h-100">
+                <li class="nav-item h-100 ">
+                    <a href="index.php"
+                        class="nav-link h-100 d-flex align-items-center jutify-content-center <?= $current_page == 'index.php' ? 'active-custom' : '' ?>">Acasă</a>
                 </li>
                 <li class="nav-item">
                     <a href="services.php"
-                        class="nav-link <?= $current_page == 'services.php' ? 'active' : '' ?>">Servicii</a>
+                        class="nav-link h-100 d-flex align-items-center jutify-content-center <?= $current_page == 'services.php' ? 'active-custom' : '' ?>">Servicii</a>
                 </li>
                 <li class="nav-item">
                     <a href="portfolio.php"
-                        class="nav-link <?= $current_page == 'portfolio.php' ? 'active' : '' ?>">Portofoliu</a>
+                        class="nav-link h-100 d-flex align-items-center jutify-content-center <?= $current_page == 'portfolio.php' ? 'active-custom' : '' ?>">Portofoliu</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item h-100 d-flex align-items-center">
                     <a href="contact.php"
-                        class="nav-link btn btn-outline-secondary px-4 mx-4 <?= $current_page == 'contact.php' ? 'active' : '' ?>">Contact</a>
+                        class="nav-link btn btn-outline-secondary px-4 mx-4 <?= $current_page == 'contact.php' ? 'active-custom' : '' ?>">Contact</a>
                 </li>
             </ul>
         </div>
