2ef18d7f5660d31488c6b67acde302be4b5d299b
Daniel Deaconescu <daniel.deaconescu98@gmail.com>
2025-09-18 06:14:25 +0300

    made the navbar responsive

--- components/navbar.php
+++ components/navbar.php
@@ -5,7 +5,7 @@
 ?>
 
 <nav class="navbar navbar-expand-lg fixed-top navbar-dark navbar-custom">
-    <div class="container h-100">
+    <div class="container container-navbar-custom h-100">
         <a href="index.html" class="navbar-brand">
             Arc Lucmar
         </a>
@@ -14,11 +14,11 @@
             aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse h-100" id="menuLinks">
-            <ul class="navbar-nav ms-auto h-100">
-                <li class="nav-item h-100 ">
+        <div class="collapse navbar-collapse navbar-collapse-custom h-100" id="menuLinks">
+            <ul class="navbar-nav ms-auto navbar-nav-custom h-100">
+                <li class="nav-item nav-item-custom h-100">
                     <a href="index.php"
-                        class="nav-link h-100 d-flex align-items-center jutify-content-center <?= $current_page == 'index.php' ? 'active-custom' : '' ?>">Acasă</a>
+                        class="nav-link nav-link-custom h-100 d-flex align-items-center jutify-content-center <?= $current_page == 'index.php' ? 'active-custom' : '' ?>">Acasă</a>
                 </li>
                 <li class="nav-item">
                     <a href="services.php"
@@ -30,9 +30,41 @@
                 </li>
                 <li class="nav-item h-100 d-flex align-items-center">
                     <a href="contact.php"
-                        class="nav-link btn btn-outline-secondary px-4 mx-4 <?= $current_page == 'contact.php' ? 'active-custom' : '' ?>">Contact</a>
+                        class="nav-link btn-contact-navbar btn btn-outline-secondary px-4 mx-4 <?= $current_page == 'contact.php' ? 'active-custom' : '' ?>">Contact</a>
                 </li>
             </ul>
         </div>
     </div>
 </nav>
+
+<!-- <nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container">
+        <a href="index.html" class="navbar-brand">
+            <img src="images/logo.svg" alt="yavin logo" class="img-fluid nav-img" />
+        </a>
+
+        <button type="button" data-bs-toggle="collapse" data-bs-target="#menuLinks" aria-controls="navbarNavDropdown"
+            aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="menuLinks">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a href="#" class="nav-link active">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Details</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Services</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Projects</a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link btn btn-outline-secondary px-4 mx-4">Contact Us</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav> -->
