b6dcd556a38f8fcdbf6c985bf22b0ddac1d8acd4
Daniel Deaconescu <daniel.deaconescu98@gmail.com>
2025-09-24 12:15:52 +0300

    restored js file

--- components/back-to-top-button.php
+++ components/back-to-top-button.php
@@ -1,5 +1,5 @@
 <div class="back-to-top-container">
-    <button type="button" class="back-to-top-buttn">
+    <button type="button" class="back-to-top-button d-none">
         <i class="fa-solid fa-chevron-up fa-2x"></i>
     </button>
 </div>
